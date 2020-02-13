26ed040982a131cc80bf2c5c4c6c58b679530483
Nikita Velichkin <velichkinns@gmail.com>
2020-02-13 13:13:38 +0600

    Update SMK-Bridging-Header.h

--- SignalMetadataKit/src/SMK-Bridging-Header.h
+++ SignalMetadataKit/src/SMK-Bridging-Header.h
@@ -4,6 +4,7 @@
 
 #import <AxolotlKit/AxolotlStore.h>
 #import <Foundation/Foundation.h>
+#import <25519/Curve25519.h>
 #import <SignalCoreKit/NSObject+OWS.h>
 #import <SignalCoreKit/OWSAsserts.h>
 #import <HKDFKit/HKDFKit.h>
